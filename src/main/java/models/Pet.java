a27fe476d81b9a0e30f0ba3b39c55e9b473ed78f
Ruslan Terehov <terehov.ruslan.dev18@gmail.com>
2020-11-17 13:07:05 +0300

    my first test with pet model

---
+++ src/main/java/models/Pet.java
@@ -0,0 +1,26 @@
+package models;
+
+public class Pet {
+    public int id;
+    public Category category;
+    public String name;
+    public String[] photoUrls;
+    public Tag[] tags;
+    public status pet_status;
+    public enum status {
+        available,
+        pending,
+        sold
+    }
+
+    public Pet( ) {
+
+    }
+
+    public Pet(int id, String name ) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+}
--- src/test/java/io/swagger/petstore/TestPet.java
+++ src/test/java/io/swagger/petstore/TestPet.java
@@ -1,17 +1,20 @@
 package io.swagger.petstore;
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.mapper.ObjectMapper;
+import models.Pet;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public class TestPet {
-    @org.junit.Test
-    public void petTest(){
+    @Test
+    public void petTest() {
         RestAssured.given()
                 .baseUri("https://petstore.swagger.io")
                 .basePath("/v2/pet")
                 .contentType(ContentType.JSON)
-                .header("api_key","asdasd1asgvvc")
+                .header("api_key", "asdasd1asgvvc")
                 .body("{" +
                         "\"id\": 12305, " +
                         "\"name\": \"doggie\"," +
@@ -21,5 +24,37 @@ public class TestPet {
                 .when().post()
                 .then()
                 .body("id", Matchers.equalTo(12305));
+    }
+
+    @Test
+    public void petModelTest() {
+        Pet pet1 = new Pet(1, "dog");
+        RestAssured.given()
+                .header("api_key", "asdasd1asgvvc")
+                .contentType("application/json")
+                .body(pet1)
+                .when()
+                .post("https://petstore.swagger.io/v2/pet");
+
+
+        Pet pet2 = new Pet(2, "cat");
+        RestAssured.given()
+                .header("api_key", "asdasd1asgvvc")
+                .contentType("application/json")
+                .body(pet2)
+                .when()
+                .post("https://petstore.swagger.io/v2/pet");
+
+
+        Pet pet_result = RestAssured.given()
+                .header("api_key", "asdasd1asgvvc")
+                .when()
+                .get("https://petstore.swagger.io/v2/pet/{pet1.id}", pet1.id)
+                .then()
+                .assertThat()
+                .extract()
+                .as(Pet.class);
+        pet_result.equals(pet1);
+
     }
 }
